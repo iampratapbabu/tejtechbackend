6d82df0dc20654a6fbde323fbe43b8d222d5c0a0
Tej Pratap <bankebihari5463@gmail.com>
2023-01-28 13:07:09 +0530

    blog delete on working

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,6 +2,7 @@ const Blog = require('../models/blogModel');
 
 exports.getAllBlogs =  async (req,res)=>{
 	const blogs = await Blog.find().populate('author_user');
+	if(!blogs){return res.status(400).json({status:"FAILURE",msg:"NO Record Found"})}
 	res.status(200).json({
 		status:"SUCCESS",
 		total_blogs:blogs.length,
@@ -9,15 +10,65 @@ exports.getAllBlogs =  async (req,res)=>{
 	})
 }
 
-exports.createBlog =  (req,res)=>{
+exports.getSingleBlog = async(req,res) =>{
+	try{
+		const blog = await Blog.findById(req.params.blogid);
+		if(!blog){return res.status(400).json({status:"FAILURE",msg:"NO Record Found"})}
+		res.status(200).json({
+			status:"SUCCESS",
+			blog
+		});
+	}catch(err){
+        res.status(500).json({
+            error: "[SERVER ERROR]",
+            errormsg: err.message
+        });
+    }
+
+}
+
+exports.createSingleBlog =  (req,res)=>{
 	try{
 		let blog={};
 		blog.title = req.body.title,
 		blog.body = req.body.body,
-		blog.author_user = req.params.user_id
+		blog.author_user = req.params.userid
 		let myblog = new Blog(blog);
 		myblog.save(); //aise blog create karte hain to save karna jaroori hota hai
-		console.log("objet blog",blog);
+		res.status(200).json({
+			status:"SUCCESS",
+			blog
+		});
+	}catch(err){
+        res.status(500).json({
+            error: "[SERVER ERROR]",
+            errormsg: err.message
+        });
+    }
+
+}
+
+exports.getMyAllBlogs = async(req,res) =>{
+	try{
+		const blogs = await Blog.find({author_user:req.params.userid})
+		if(blogs.length<=0){return res.status(400).json({status:"FAILURE",msg:"NO Record Found"})}
+		res.status(200).json({
+			status:"SUCCESS",
+			blogs
+		});
+	}catch(err){
+        res.status(500).json({
+            error: "[SERVER ERROR]",
+            errormsg: err.message
+        });
+    }
+
+}
+
+exports.getMySingleBlog = async(req,res) =>{
+	try{
+		const blog = await Blog.find({_id:req.params.blogid},{user:req.params.userid}).select('title body');
+		if(!blog){return res.status(400).json({status:"FAILURE",msg:"NO Record Found"})}
 		res.status(200).json({
 			status:"SUCCESS",
 			blog
@@ -32,25 +83,7 @@ exports.createBlog =  (req,res)=>{
 }
 
 
-
-exports.getSingleBlog = (req,res) =>{
-	try{
-
-		res.status(200).json({
-			status:"SUCCESS",
-			blog
-		});
-	}catch(err){
-        res.status(500).json({
-            error: "[SERVER ERROR]",
-            errormsg: err.message
-        });
-    }
-
-}
-
-
-exports.updateSingleBlog = (req,res) =>{
+exports.updateSingleBlog = async(req,res) =>{
 	try{
 
 		res.status(200).json({
@@ -65,9 +98,14 @@ exports.updateSingleBlog = (req,res) =>{
     }
 }
 
-exports.deleteSingleBlog = (req,res) =>{
+exports.deleteSingleBlog = async(req,res) =>{
 	try{
-
+		const blog = await Blog.find({_id:req.params.blogid},{user:req.params.userid});
+		if(!blog){return res.status(400).json({status:"FAILURE",msg:"NO Record Found"})}
+		console.log(blog[0])
+		console.log(blog[0].user,req.params.userid)
+		if(blog[0].user != req.params.userid){return res.status(400).json({status:"FAILURE",msg:"Your are Not Authorized"})}
+		blog.remove();
 		res.status(200).json({
 			status:"SUCCESS",
 			blog
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -8,13 +8,21 @@ const router = express.Router();
 router.route('/')
 .get(blogController.getAllBlogs)
 
-router.route('/create-blog/:user_id')
-.post(blogController.createBlog)
-
-
-router.route('/single/:id')
+router.route('/single/:blogid')
 .get(blogController.getSingleBlog)
+
+router.route('/myblog/:userid')
+.get(blogController.getMyAllBlogs)
+.post(blogController.createSingleBlog)
+
+router.route('/myblog/:userid/:blogid')
+.get(blogController.getMySingleBlog)
 .patch(blogController.updateSingleBlog)
 .delete(blogController.deleteSingleBlog);
+
+
+
+
+
 
 module.exports = router;
