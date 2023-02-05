32c325ad4ce22099f6ed24a6bcf5c2247afe8030
Tej Pratap <bankebihari5463@gmail.com>
2023-02-05 20:14:23 +0530

    up to authentication

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if(process.env.NODE_ENV="development"){
 app.use((req,res,next) =>{
 	//res.header("Access-Control-Allow-Credentials","true");
 	res.header("Access-Control-Allow-Origin","*");
-	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,x-auth-token, Content-Type, Accept,Authorization");
+	res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,x-auth-token, Content-Type, Accept,Authorization,x-access-token");
 	res.header("Access-Control-Allow-Methods","GET,OPTIONS,POST,PUT,PATCH,DELETE");
 	next();
 });
