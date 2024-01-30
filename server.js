687e7d5e2badc1d1b2be5963d62b68b4fb0fbf93
Tej Pratap <bankebihari5463@gmail.com>
2024-01-30 23:39:13 +0530

    fix

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const mongoose  = require('mongoose');
+const path = require('path'); 
+
 const app = require('./app');
 
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: path.join(__dirname, 'config.env') });
 
 const DB = process.env.DATABASE;
 mongoose
   .connect(DB, {
-  	dbName: `tejtechdb`,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+  	dbName: `tejtechdb`,  //either we can specify db name here or we can put it in connection string here wil be checked first
+                          //#mongodb+srv://tejpratap:tejtech@cluster0.qmxv4cn.mongodb.net/===>tejtech_prod<===?retryWrites=true&w=majority
+                        //if we didn't specify any it will create a db with name of test
   })
   .then(() => {
     console.log("database connected successfully");
+  }).catch(err =>{
+    console.log("ERR CONNECTING DATABASE",err);
   });
 
 
