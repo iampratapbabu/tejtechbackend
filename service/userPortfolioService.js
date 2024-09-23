289e874fe0f64867469f8f3a7d40da99d6f3ec34
Tej Pratap <bankebihari5463@gmail.com>
2024-09-24 01:02:09 +0530

    change

--- service/userPortfolioService.js
+++ service/userPortfolioService.js
@@ -8,18 +8,35 @@ const Stock = require('../models/stockModel');
 exports.userPortfolioSummary = async (req) => {
     try {
         let userPortfolioSummary = {
-            "netWorth": 0,
-            "totalAssets": 0,
-            "totalLiablites": 0,
-            // "assets":[],
-            // "liablities":[],
+            netWorth: 0,
+            totalAssets: 0,
+            totalLiablites: 0,
+            userAssests: [
+                {
+                    assetType: "Savings Account",
+                    assetValue: 0,
+                    assetDate: ""
+                },
+                {
+                    assetType: "Mutual Funds",
+                    assetValue: 0,
+                    assetDate: ""
+                },
+                {
+                    assetType: "Stocks",
+                    assetValue: 0,
+                    assetDate: ""
+                }
+            ],
+            userLoans: [],
+            userExpenses: [],
         }
 
         console.log(req.user);
         let userMutualFunds = await MutualFund.find({ user: req.user._id });
         let userStocks = await Stock.find({ user: req.user._id });
         let userBankAccounts = await BankAccount.find({ user: req.user._id });
-        let userExpenses = await PersonalExpense.find({ user: req.user._id });
+        let userExpenses = await PersonalExpense.find({ user: req.user._id }).sort('-createdAt');
         let userLoans = await Loan.find({ user: req.user._id });
 
         let totalMf = 0, totalStocks = 0, totalBankBalance = 0, totalExpense = 0, totalLoans = 0;
@@ -45,9 +62,33 @@ exports.userPortfolioSummary = async (req) => {
             totalLoans += loan?.amount;
         }
 
-        userPortfolioSummary.totalAssets = totalMf + totalBankBalance + totalStocks;
-        userPortfolioSummary.totalLiablites = totalLoans;
-        userPortfolioSummary.netWorth = (totalMf + totalBankBalance + totalStocks) - (totalLoans);
+        const totalAssestsValue = totalMf + totalBankBalance + totalStocks;
+        const totalLiablitesValue = totalLoans + totalExpense;
+        const netWorthValue = totalAssestsValue - totalLiablitesValue;
+
+        userPortfolioSummary.totalAssets = totalAssestsValue;
+        userPortfolioSummary.totalLiablites = totalLiablitesValue;
+        userPortfolioSummary.netWorth = netWorthValue;
+
+        for (let i = 0; i < userPortfolioSummary.userAssests.length; i++) {
+            if (userPortfolioSummary.userAssests[i].assetType === "Savings Account") {
+                userPortfolioSummary.userAssests[i].assetValue = totalBankBalance;
+                    userPortfolioSummary.userAssests[i].assetDate = new Date();
+            }
+            if (userPortfolioSummary.userAssests[i].assetType === "Mutual Funds") {
+                userPortfolioSummary.userAssests[i].assetValue = totalMf;
+                    userPortfolioSummary.userAssests[i].assetDate = new Date();
+            }
+            if (userPortfolioSummary.userAssests[i].assetType === "Stocks") {
+                userPortfolioSummary.userAssests[i].assetValue = totalStocks;
+                    userPortfolioSummary.userAssests[i].assetDate = new Date();
+            }
+        }
+
+        userPortfolioSummary.userLoans = userLoans;
+        userPortfolioSummary.userExpenses = userExpenses;
+
+
 
         return userPortfolioSummary;
 
